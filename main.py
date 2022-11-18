b776442647ccfb61dd48754c2c3474fea9da52be
SYN <SYN@mail.com>
2022-11-18 19:37:00 +0530

    added color module for printing colored output.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 
 import random
 
+import colorama
+from colorama import Fore, Back, Style # |> importing colorama for styling output
+
+colorama.init(autoreset=True)
+
+"""
+Fore : RED, BLACK, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+Back : RED, BLACK, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+Style : DIM, NORMAL, BRIGHT, RESET_ALL.
+"""
+
+
 exit = False
 user_point = 0
 computer_point = 0
@@ -8,7 +20,7 @@ computer_point = 0
 
 while exit == False:
     options = ["rock", "paper", "scissor"]
-    user_input = input('Choose rock, paper, scissor or exit :  \n')
+    user_input = input(Fore.CYAN + Style.BRIGHT + 'Choose rock, paper, scissor or exit :  \n')
     computer_input = random.choice(options)
 
     if user_input == 'exit':
@@ -20,54 +32,54 @@ while exit == False:
         if computer_input == 'rock':
             print('your input is Rock')
             print('Computer input is Rock')
-            print('It is a tie \n')
+            print(Fore.YELLOW + Style.BRIGHT + 'It is a tie \n')
 
         elif computer_input == 'paper':
             print('your input is Rock')
             print('Computer input is Paper')
-            print('Computer wins \n')
+            print(Fore.RED + Style.BRIGHT + 'Computer wins \n')
             computer_point +=1
 
         elif computer_input == 'scissor':
             print('your input is Rock')
             print('Computer input is Scissor')
-            print('You win \n')
+            print(Fore.GREEN + Style.BRIGHT + 'You win \n')
             user_point +=1
 
     elif user_input == 'paper':
         if computer_input == 'rock':
             print('your input is Paper')
             print('Computer input is Rock')
-            print('You win \n')
+            print(Fore.GREEN + Style.BRIGHT + 'You win \n')
             user_point +=1
 
         elif computer_input == 'paper':
             print('your input is Paper')
             print('Computer input is Paper')
-            print('Its a tie')
+            print(Fore.YELLOW + Style.BRIGHT + 'Its a tie')
 
         elif computer_input == 'scissor':
             print('your input is Paper')
             print('Computer input is Scissor')
-            print('Computer wins \n')
+            print(Fore.RED + Style.BRIGHT + 'Computer wins \n')
             computer_point +=1
     elif user_input == 'scissor':
         if computer_input == 'rock':
             print('your input is scissor')
             print('Computer input is Rock')
-            print('Computer wins \n')
+            print(Fore.RED + Style.BRIGHT + 'Computer wins \n')
             computer_point +=1
 
         elif computer_input == 'paper':
             print('your input is scissor')
             print('Computer input is Paper')
-            print('You win \n')
+            print(Fore.GREEN + Style.BRIGHT + 'You win \n')
             user_point +=1
 
         elif computer_input == 'scissor':
             print('your input is scissor')
             print('Computer input is Scissor')
-            print('Its a tie \n')
+            print(Fore.YELLOW + Style.BRIGHT + 'Its a tie \n')
 
     elif user_input != 'rock' or user_input != 'paper' or user_input !=  'scissor':
-        print('Invalid input \n')
+        print(Back.RED + 'Invalid input \n')
